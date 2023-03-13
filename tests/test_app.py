28a3c01ab683f595be8659740dd43740e6879b3e
Dominik Pioś <792954018@wp.pl>
2023-03-13 17:13:56 +0100

    Register router tests
    Request body unit tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,7 @@
-from aws_spy import ServerlessConfig, SpyAPI, __version__
+import pytest
+
+from aws_spy import ServerlessConfig, SpyAPI, SpyRouter, __version__
+from aws_spy.core.schemas import Methods
 
 
 def test_app() -> None:
@@ -7,3 +10,29 @@ def test_app() -> None:
 
 def test_app_conf(app: SpyAPI, config: ServerlessConfig) -> None:
     assert app.config == config
+
+
+@pytest.mark.parametrize("prefix", ["api", "/api", "/somet_other_prefix"])
+def test_prefix(config: ServerlessConfig, prefix: str) -> None:
+    app = SpyAPI(config=config, prefix=prefix)
+    expected_prefix = prefix if prefix.startswith("/") else f"/{prefix}"
+    assert app.prefix == expected_prefix
+
+
+@pytest.mark.parametrize("method", [method for method in Methods])
+def test_register_router(config: ServerlessConfig, method: Methods) -> None:
+    app = SpyAPI(config=config, prefix="/api")
+    router = SpyRouter(prefix="/router")
+    router_method = getattr(router, method.value)
+
+    @router_method("path", "lambda")
+    def handler() -> None:
+        ...
+
+    assert len(router.routes) == 1
+    assert len(app.routes) == 0
+    assert router.routes["/router/path"][method]
+
+    app.register_router(router)
+    assert len(app.routes) == 1
+    assert app.routes["/api/router/path"][method]
--- tests/unit/test_request_body.py
+++ tests/unit/test_request_body.py
@@ -3,18 +3,42 @@ from pydantic import BaseModel
 
 from aws_spy import SpyAPI
 from aws_spy.core.exceptions import RouteDefinitionException
+from aws_spy.core.schemas import Methods
 
 
 class Request(BaseModel):
     ...
 
 
-def test_get_method_with_request_body(app: SpyAPI) -> None:
+@pytest.mark.parametrize(
+    "method",
+    [method for method in Methods if method in (Methods.GET, Methods.DELETE)],
+)
+def test_get_method_with_request_body(app: SpyAPI, method: Methods) -> None:
+    app_method = getattr(app, method.value)
     with pytest.raises(
         RouteDefinitionException,
-        match='GET method on "/some_path" cannot have request body!',
+        match=f'{method.upper()} method on "/some_path" cannot have request body!',
     ):
 
-        @app.get("/some_path", "test-lambda")
+        @app_method("/some_path", "test-lambda")
         def handler(request: Request) -> None:
             ...
+
+
+@pytest.mark.parametrize(
+    "method",
+    [method for method in Methods if method not in (Methods.GET, Methods.DELETE)],
+)
+def test_request_body(app: SpyAPI, method: Methods) -> None:
+    app_method = getattr(app, method.value)
+    path = "/somepath"
+
+    @app_method(path, "lambda")
+    def handler(request: Request) -> None:
+        ...
+
+    assert len(app.routes) == 1
+    route = app.routes[path][method]
+    assert route.request_body == Request
+    assert route.request_body_arg_name == "request"
